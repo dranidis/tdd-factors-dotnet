30923eaff1fc1bb8abbee57ed187d57bd9c6a76f
D Dranidis <dranidis@gmail.com>
2024-01-30 23:58:59 +0200

    Add a general test case

--- test/PrimeFactors.Test/PrimeUtilTest.cs
+++ test/PrimeFactors.Test/PrimeUtilTest.cs
@@ -10,6 +10,8 @@ public class PrimeUtilTest
     [DataRow(4, new int[] { 2, 2 })]
     [DataRow(8, new int[] { 2, 2, 2 })]
     [DataRow(9, new int[] { 3, 3 })]
+    [DataRow(2 * 3 * 3 * 5 * 7 * 11 * 13 * 13,
+    new int[] { 2, 3, 3, 5, 7, 11, 13, 13 })]
     public void GetPrimeFactors_Returns_ListWithPrimeFactorsOfTheProvidedNumber(int number, int[] intArrayExpected)
     {
         // Arrange
